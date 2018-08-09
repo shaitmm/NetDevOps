32ccf6951deefa765b2fe68224d7805440ef3599
Xin Yu <xinyu3@cisco.com>
2018-08-09 11:27:29 +0800

    change some files

---
+++ script/.ipynb_checkpoints/login-checkpoint.py
@@ -0,0 +1,119 @@
+import pexpect
+from pexpect import EOF, TIMEOUT
+import sys
+
+class DeviceBase(object):
+
+    def __init__(self, params):
+
+        self.params = params
+        self.hostname = params.get("hostname")
+        self.port = params.get("port", 22)
+        self.username = params.get("username", "lab")
+        self.password = params.get("password", "lab")
+        self.connect_type = params.get("connect_type", "ssh")
+        self.device_type = params.get("device_type", "iosxr")
+
+        self.debug = params.get("debug", 0)
+        self.verbose = params.get("verbose", False)
+
+
+        self.expect_list = []
+
+
+    def _telnet(self):
+
+        cmd = "telnet {hostname} {port}".format(hostname=self.hostname, port=self.port)
+        return cmd
+
+
+    def _ssh(self):
+        ssh = "ssh {hostname} -l {username} " \
+              " -p {port} " \
+              "-o StrictHostKeyChecking=no " \
+              "-o UserKnownHostsFile=/dev/null".format(username=self.username,
+                                                       hostname=self.hostname,
+                                                       port=self.port)
+        return ssh
+
+    def connect(self, timeout=5):
+        if self.connect_type.lower() == "ssh":
+            self.c =pexpect.spawn(self._ssh(), encoding="utf-8")
+
+        elif self.connect_type.lower() == "telnet":
+            self.c = pexpect.spawn(self._telnet(), encoding="utf-8")
+        else:
+            raise ("connect error")
+
+        self.c.delaybeforesend = 1
+        self.c.timeout = timeout
+        self.c.logfile = sys.stdout
+
+        return self.c
+
+
+
+    def login(self, prompt=r"[>|#|$]\s?$"):
+        self.expect_list = []
+        self.expect_list.append(r"(?i)username[:]?\s*$")    # username
+        self.expect_list.append(r"(?i)login[:]?\s*$")       # login
+        self.expect_list.append(r"(?i)password[:]?\s*$")    # password
+        # self.expect_list.append(r"secret:\s*$")           # IOS-XR init config
+        self.expect_list.append(prompt)
+        self.c.send("\n\r")
+        for _ in range(0, 3):
+            result = []
+
+            try:
+                i = self.c.expect(self.expect_list, timeout=5)
+                result.append(i)
+                result.append(str(self.c.before))
+                result.append(str(self.c.after))
+                if i == 0 or i == 1:
+                    self.c.sendline(self.username)
+                elif i == 2:
+                    self.c.sendline(self.password)
+                elif i == 3:
+                    break
+            except EOF:
+                break
+            except TIMEOUT:
+                print("connect to %s timeout" % self.hostname)
+                break
+
+        if result[0] < 3:
+            print(result)
+            print("username or password error")
+            return result
+
+        self._set_terminal_length_zero()
+        return result
+
+    def _set_terminal_length_zero(self):
+        pass
+
+    def logout(self):
+        if self.c:
+            self.c.terminate()
+
+    def __del__(self):
+        self.logout()
+
+
+
+
+if __name__ == "__main__":
+
+    device = {"hostname": "172.20.0.1",
+              "port":33281,
+              "username": "yuxin",
+              "password": "yuxin123",
+              "connect_type": "telnet",
+              "device_type": "iosxr"}
+    d = DeviceBase(device)
+    d.connect()
+    d.login()
+
+
+
+
---
+++ script/.ipynb_checkpoints/setmgt_ip-checkpoint.py
@@ -0,0 +1,41 @@
+import sys
+sys.path.append("../lib")
+
+from telnet import Telnet
+
+iosxr = [{'port': 33281, 'hostname': 'xrv1', 'username': 'admin', 'password': 'admin',"mgt_ip": "172.20.3.101"},
+                {'port': 33282, 'hostname': 'xrv2', 'username': 'admin', 'password': 'admin',"mgt_ip": "172.20.3.102"},
+                {'port': 33283, 'hostname': 'xrv3', 'username': 'admin', 'password': 'admin',"mgt_ip": "172.20.3.103"},
+                {'port': 33284, 'hostname': 'xrv4', 'username': 'admin', 'password': 'admin',"mgt_ip": "172.20.3.104"},
+                {'port': 33285, 'hostname': 'xrv5', 'username': 'admin', 'password': 'admin',"mgt_ip": "172.20.3.105"},
+                {'port': 33286, 'hostname': 'xrv6', 'username': 'admin', 'password': 'admin',"mgt_ip": "172.20.3.106"},
+                {'port': 33287, 'hostname': 'xrv7', 'username': 'admin', 'password': 'admin',"mgt_ip": "172.20.3.107"},
+                {'port': 33288, 'hostname': 'xrv8', 'username': 'admin', 'password': 'admin',"mgt_ip": "172.20.3.108"},
+                {'port': 33289, 'hostname': 'xrv9', 'username': 'admin', 'password': 'admin',"mgt_ip": "172.20.3.109"},
+                {'port': 33290, 'hostname': 'xrv10', 'username': 'admin', 'password': 'admin',"mgt_ip": "172.20.3.110"},
+                {'port': 33291, 'hostname': 'xrv11', 'username': 'admin', 'password': 'admin',"mgt_ip": "172.20.3.111"},
+                {'port': 33292, 'hostname': 'xrv12', 'username': 'admin', 'password': 'admin',"mgt_ip": "172.20.3.112"},
+                {'port': 33293, 'hostname': 'xrv13', 'username': 'admin', 'password': 'admin',"mgt_ip": "172.20.3.113"},
+                {'port': 33294, 'hostname': 'xrv14', 'username': 'admin', 'password': 'admin',"mgt_ip": "172.20.3.114"},
+                {'port': 33295, 'hostname': 'xrv15', 'username': 'admin', 'password': 'admin',"mgt_ip": "172.20.3.115"},
+               ]
+
+
+vios = [{'port': 33296, 'hostname': 'vIOS16', 'username': 'admin', 'password': 'admin', "mgt_ip": "172.20.3.116"},
+                {'port': 33297, 'hostname': 'vIOS17', 'username': 'admin', 'password': 'admin', "mgt_ip": "172.20.3.117"},
+                {'port': 33298, 'hostname': 'vIOS18', 'username': 'admin', 'password': 'admin', "mgt_ip": "172.20.3.118"},
+                {'port': 33299, 'hostname': 'vIOS19', 'username': 'admin', 'password': 'admin', "mgt_ip": "172.20.3.119"},
+                {'port': 33300, 'hostname': 'vIOS20', 'username': 'admin', 'password': 'admin', "mgt_ip": "172.20.3.120"},
+                {'port': 33301, 'hostname': 'vIOS21', 'username': 'admin', 'password': 'admin', "mgt_ip": "172.20.3.121"},
+               ]
+
+##  set xrvx mgt interface ip:
+for d in devices_info:
+    device_type =""
+    if "xrv" in d.get("hostname"):
+        device_type = "iosxr"
+    if "IOS" in d.get("hostname"):
+        device_type = "ios" 
+    tn = Telnet(host="172.20.0.1", port=d.get("port"), device_type=device_type, debug=True)
+    tn.send_config_set(d.get("hostname"))
+
